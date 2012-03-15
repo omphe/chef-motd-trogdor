1574e1a42b803406efafa9d0b66a99deaf9d31e2
Branden Faulls <branden.faulls@clockworkrobot.co.uk>
2012-03-15 22:37:41 +0000

    Initial checkin

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Branden Faulls"
+maintainer_email  "chef-motd-trogdor@clockworkrobot.co.uk"
+license           "MIT"
+description       "Sets up the login message of the day to display Trogdor, the burninator"
+long_description  "Sets up the login message of the day to display Trogdor, the burninator"
+version           "0.1.0"
+recipe            "motd_trogdor", "Display Trogdor in the motd"
+
